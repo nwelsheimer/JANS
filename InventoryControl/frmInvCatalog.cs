9096dff69710af1b366bea588eab486289cf594b
Nick Welsheimer <micros_joy@hotmail.com>
2017-04-28 15:11:39 -0400

    Added inventory control project

---
+++ InventoryControl/frmInvCatalog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using MetroFramework.Forms;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryControl
+{
+    public partial class frmInvCatalog : MetroForm
+    {
+        public frmInvCatalog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InventoryControl/frmInvHeight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroFramework.Forms;
+using System.Windows.Forms;
+
+namespace InventoryControl
+{
+    public partial class frmInvHeight : MetroForm
+    {
+        public frmInvHeight()
+        {
+            InitializeComponent();
+        }
+    }
+}
