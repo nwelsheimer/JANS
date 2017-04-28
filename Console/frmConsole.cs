e72ff675ed61075741c148c8e70bc8986e6bbbc6
Nick Welsheimer <micros_joy@hotmail.com>
2017-04-28 14:10:58 -0400

    Commit

---
+++ Console/frmConsole.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroFramework.Forms;
+using System.Windows.Forms;
+using General;
+
+namespace ConsoleCtl
+{
+    public partial class frmConsole : MetroForm
+    {
+        public frmConsole()
+        {
+            InitializeComponent();
+            this.Location = new Point(50, 50);
+            cnConsole.InternalRichTextBox.KeyUp += ConsoleKeyDown;
+        }
+
+        private void frmConsole_Load(object sender, EventArgs e)
+        {
+            cnConsole.InternalRichTextBox.ReadOnly = false;
+            cnConsole.Focus();
+            prompt();
+        }
+        private void ConsoleKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string input = getInput();
+                string[] inputParams = input.Split(' ');
+
+                switch (inputParams[0])
+                {
+                    case "show":
+                        cnConsole.WriteOutput(show(inputParams[1]),Color.Red);
+                        prompt();
+                        break;
+                    case "exit":
+                        prompt();
+                        this.Hide();
+                        break;
+                    case "quit":
+                        this.Close();
+                        break;
+                    case "debug":
+                        debugging();
+                        prompt();
+                        break;
+                    default:
+                        cnConsole.WriteOutput("Invalid command, type 'help' for list of commands.\n", Color.Red);
+                        prompt();
+                        break;
+                }
+                    
+            }
+        }
+        private void debugging()
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            while (!p.HasExited)
+                cnConsole.WriteOutput(p.StandardOutput.ReadLine() + "\n", Color.Red);
+            cnConsole.WriteOutput("\n", Color.Red);
+        }
+        private string show(string input)
+        {
+            string output = "Cannot find variable " + input + ".\n";
+            switch (input)
+            {
+                case "constring":
+                    output = Global.SQLCON != null ? Global.SQLCON + "\n" : "Connection string not set yet.\n";
+                    break;
+            }
+            return output;
+        }
+        private void prompt()
+        {
+            cnConsole.WriteOutput("jans>", Color.Red);
+        }
+        private string getInput()
+        {
+            string input = "     ";
+            int line = cnConsole.InternalRichTextBox.Lines.Length - 2;
+            while (input.Substring(0,5) != "jans>")
+            {
+                input = cnConsole.InternalRichTextBox.Lines[line];
+                line--;
+            }
+            return input.Substring(5, input.Length - 5);
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
---
+++ Console/frmConsole.designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ConsoleCtl
+{
+    partial class frmConsole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cnConsole = new ConsoleControl.ConsoleControl();
+            this.lblClose = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // cnConsole
+            // 
+            this.cnConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cnConsole.IsInputEnabled = true;
+            this.cnConsole.Location = new System.Drawing.Point(3, 31);
+            this.cnConsole.Name = "cnConsole";
+            this.cnConsole.SendKeyboardCommandsToProcess = false;
+            this.cnConsole.ShowDiagnostics = false;
+            this.cnConsole.Size = new System.Drawing.Size(865, 550);
+            this.cnConsole.TabIndex = 1;
+            // 
+            // lblClose
+            // 
+            this.lblClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblClose.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblClose.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.lblClose.Location = new System.Drawing.Point(843, 7);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(23, 21);
+            this.lblClose.TabIndex = 2;
+            this.lblClose.Text = "X";
+            this.lblClose.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel2.Location = new System.Drawing.Point(378, 7);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(114, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "System Console";
+            this.metroLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmConsole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 582);
+            this.ControlBox = false;
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.cnConsole);
+            this.Name = "frmConsole";
+            this.Resizable = false;
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.Load += new System.EventHandler(this.frmConsole_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ConsoleControl.ConsoleControl cnConsole;
+        private MetroFramework.Controls.MetroLabel lblClose;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
