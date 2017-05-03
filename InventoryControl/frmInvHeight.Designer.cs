d69a470cb2ae577c0fa21a960254287f1557ba95
Nick Welsheimer <micros_joy@hotmail.com>
2017-05-03 16:34:42 -0400

    More changes

--- InventoryControl/frmInvHeight.Designer.cs
+++ InventoryControl/frmInvHeight.Designer.cs
@@ -31,9 +31,24 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance3 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance4 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance5 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance6 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance7 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance8 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance9 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance10 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance11 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance12 = new Infragistics.Win.Appearance();
             this.grdHeight = new MetroFramework.Controls.MetroGrid();
-            this.Height = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmbSite = new MetroFramework.Controls.MetroComboBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.ugrdHeights = new Infragistics.Win.UltraWinGrid.UltraGrid();
             ((System.ComponentModel.ISupportInitialize)(this.grdHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ugrdHeights)).BeginInit();
             this.SuspendLayout();
             // 
             // grdHeight
@@ -42,7 +57,12 @@
             this.grdHeight.AllowUserToDeleteRows = false;
             this.grdHeight.AllowUserToOrderColumns = true;
             this.grdHeight.AllowUserToResizeRows = false;
+            this.grdHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdHeight.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.grdHeight.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.grdHeight.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.grdHeight.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.grdHeight.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -54,8 +74,6 @@
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.grdHeight.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.grdHeight.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdHeight.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Height});
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
@@ -67,7 +85,7 @@
             this.grdHeight.EnableHeadersVisualStyles = false;
             this.grdHeight.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.grdHeight.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.grdHeight.Location = new System.Drawing.Point(7, 67);
+            this.grdHeight.Location = new System.Drawing.Point(23, 378);
             this.grdHeight.Name = "grdHeight";
             this.grdHeight.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -80,31 +98,117 @@
             this.grdHeight.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.grdHeight.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.grdHeight.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grdHeight.Size = new System.Drawing.Size(1017, 500);
+            this.grdHeight.Size = new System.Drawing.Size(994, 246);
             this.grdHeight.TabIndex = 0;
             // 
-            // Height
+            // cmbSite
             // 
-            this.Height.HeaderText = "Height";
-            this.Height.Name = "Height";
+            this.cmbSite.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbSite.FormattingEnabled = true;
+            this.cmbSite.ItemHeight = 23;
+            this.cmbSite.Location = new System.Drawing.Point(853, 46);
+            this.cmbSite.Name = "cmbSite";
+            this.cmbSite.Size = new System.Drawing.Size(164, 29);
+            this.cmbSite.TabIndex = 1;
+            this.cmbSite.UseSelectable = true;
+            this.cmbSite.SelectionChangeCommitted += new System.EventHandler(this.cmbSite_SelectionChangeCommitted);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(766, 51);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(81, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Choose Site:";
+            // 
+            // ugrdHeights
+            // 
+            appearance1.BackColor = System.Drawing.SystemColors.Window;
+            appearance1.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ugrdHeights.DisplayLayout.Appearance = appearance1;
+            this.ugrdHeights.DisplayLayout.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.ugrdHeights.DisplayLayout.CaptionVisible = Infragistics.Win.DefaultableBoolean.False;
+            appearance2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            appearance2.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance2.BackGradientStyle = Infragistics.Win.GradientStyle.Vertical;
+            appearance2.BorderColor = System.Drawing.SystemColors.Window;
+            this.ugrdHeights.DisplayLayout.GroupByBox.Appearance = appearance2;
+            appearance3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.ugrdHeights.DisplayLayout.GroupByBox.BandLabelAppearance = appearance3;
+            this.ugrdHeights.DisplayLayout.GroupByBox.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            appearance4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            appearance4.BackColor2 = System.Drawing.SystemColors.Control;
+            appearance4.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.ugrdHeights.DisplayLayout.GroupByBox.PromptAppearance = appearance4;
+            this.ugrdHeights.DisplayLayout.MaxColScrollRegions = 1;
+            this.ugrdHeights.DisplayLayout.MaxRowScrollRegions = 1;
+            appearance5.BackColor = System.Drawing.SystemColors.Window;
+            appearance5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ugrdHeights.DisplayLayout.Override.ActiveCellAppearance = appearance5;
+            appearance6.BackColor = System.Drawing.SystemColors.Highlight;
+            appearance6.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.ugrdHeights.DisplayLayout.Override.ActiveRowAppearance = appearance6;
+            this.ugrdHeights.DisplayLayout.Override.BorderStyleCell = Infragistics.Win.UIElementBorderStyle.Dotted;
+            this.ugrdHeights.DisplayLayout.Override.BorderStyleRow = Infragistics.Win.UIElementBorderStyle.Dotted;
+            appearance7.BackColor = System.Drawing.SystemColors.Window;
+            this.ugrdHeights.DisplayLayout.Override.CardAreaAppearance = appearance7;
+            appearance8.BorderColor = System.Drawing.Color.Silver;
+            appearance8.TextTrimming = Infragistics.Win.TextTrimming.EllipsisCharacter;
+            this.ugrdHeights.DisplayLayout.Override.CellAppearance = appearance8;
+            this.ugrdHeights.DisplayLayout.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText;
+            this.ugrdHeights.DisplayLayout.Override.CellPadding = 0;
+            appearance9.BackColor = System.Drawing.SystemColors.Control;
+            appearance9.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance9.BackGradientAlignment = Infragistics.Win.GradientAlignment.Element;
+            appearance9.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance9.BorderColor = System.Drawing.SystemColors.Window;
+            this.ugrdHeights.DisplayLayout.Override.GroupByRowAppearance = appearance9;
+            appearance10.TextHAlignAsString = "Left";
+            this.ugrdHeights.DisplayLayout.Override.HeaderAppearance = appearance10;
+            this.ugrdHeights.DisplayLayout.Override.HeaderClickAction = Infragistics.Win.UltraWinGrid.HeaderClickAction.SortMulti;
+            this.ugrdHeights.DisplayLayout.Override.HeaderStyle = Infragistics.Win.HeaderStyle.WindowsXPCommand;
+            appearance11.BackColor = System.Drawing.SystemColors.Window;
+            appearance11.BorderColor = System.Drawing.Color.Silver;
+            this.ugrdHeights.DisplayLayout.Override.RowAppearance = appearance11;
+            this.ugrdHeights.DisplayLayout.Override.RowSelectors = Infragistics.Win.DefaultableBoolean.False;
+            appearance12.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ugrdHeights.DisplayLayout.Override.TemplateAddRowAppearance = appearance12;
+            this.ugrdHeights.DisplayLayout.ScrollBounds = Infragistics.Win.UltraWinGrid.ScrollBounds.ScrollToFill;
+            this.ugrdHeights.DisplayLayout.ScrollStyle = Infragistics.Win.UltraWinGrid.ScrollStyle.Immediate;
+            this.ugrdHeights.DisplayLayout.ViewStyleBand = Infragistics.Win.UltraWinGrid.ViewStyleBand.OutlookGroupBy;
+            this.ugrdHeights.Location = new System.Drawing.Point(23, 81);
+            this.ugrdHeights.Name = "ugrdHeights";
+            this.ugrdHeights.Size = new System.Drawing.Size(994, 291);
+            this.ugrdHeights.TabIndex = 3;
+            this.ugrdHeights.Text = "ultraGrid1";
             // 
             // frmInvHeight
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1031, 585);
+            this.ClientSize = new System.Drawing.Size(1040, 647);
+            this.Controls.Add(this.ugrdHeights);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.cmbSite);
             this.Controls.Add(this.grdHeight);
             this.Name = "frmInvHeight";
             this.Text = "Inventory Height Control";
             ((System.ComponentModel.ISupportInitialize)(this.grdHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ugrdHeights)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private MetroFramework.Controls.MetroGrid grdHeight;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Height;
+        private MetroFramework.Controls.MetroComboBox cmbSite;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private Infragistics.Win.UltraWinGrid.UltraGrid ugrdHeights;
     }
 }
 
--- InventoryControl/frmInvHeight.cs
+++ InventoryControl/frmInvHeight.cs
@@ -15,17 +15,34 @@ namespace InventoryControl
 {
     public partial class frmInvHeight : MetroForm
     {
-        DataTable itemHeights = new DataTable();
-        MetroFramework.Components.MetroStyleManager sm = metroTools.setStyles();
+        DataTable itemHeights = new DataTable(); //local working dataset
+        MetroFramework.Components.MetroStyleManager sm = metroTools.setStyles(); //JANS local style manager
 
         public frmInvHeight()
         {
             InitializeComponent();
-            Global.connectToDB();
-            itemHeights = Global.GetData("usp_INV_SelectHeights @siteId=33").Tables[0];
-            this.StyleManager = sm;
+            Global.connectToDB(); //faster method for setting up DB connectivity in JANS. Use this going forward
+
+            this.StyleManager = sm; //Assign the style manager to the form
             sm.Owner = this;
+
+            cmbSite.DataSource = Global.GetData("usp_SYS_GetSites").Tables[0]; //Set up site combo
+            cmbSite.ValueMember = "id";
+            cmbSite.DisplayMember = "description";
+            updateGrid();
+        }
+
+        private void updateGrid()
+        {
+            if (cmbSite.SelectedValue.ToString() != "")
+                itemHeights = Global.GetData("usp_INV_SelectHeights @siteId=" + cmbSite.SelectedValue).Tables[0];
             grdHeight.DataSource = itemHeights;
+            ugrdHeights.DataSource = itemHeights;
+        }
+        
+        private void cmbSite_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            updateGrid();
         }
     }
 }
